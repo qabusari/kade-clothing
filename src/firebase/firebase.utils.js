9afab6467d055adbb5430525b1e7b5e3fc42177f
qabusari <quddusbusari@outlook.com>
2022-02-25 20:38:31 +0100

    integrate firebase and google sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,48 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyCGYFnqA31n3ZoBE9cimn7qLwRx7fwMJVI",
+  authDomain: "kade-db.firebaseapp.com",
+  projectId: "kade-db",
+  storageBucket: "kade-db.appspot.com",
+  messagingSenderId: "766562034737",
+  appId: "1:766562034737:web:b84eaa426765dbdcdde110",
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
